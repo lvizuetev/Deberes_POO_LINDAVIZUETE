c4a4e3c2ceb0c95e3b2b63fd14c75db06f35bcab
lvizuetev <85849274+lvizuetev@users.noreply.github.com>
2022-01-07 01:18:03 -0500

    Add files via upload

---
+++ Tarea3_POO_LINDA_VIZUETE/js/Ejercicios_v1.js
@@ -0,0 +1,23 @@
+//*1) Programa una función que cuente el número de caracteres 
+//*de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
+/* 
+let respuesta = prompt("introduce texto  para medir su longitud");
+
+const contarCaracteres = (cadena) => {
+    try {
+        
+        if (typeof(cadena) !== "string") throw "ERROR: Cadena no válida, inténtalo de nuevo";
+        else respuesta=cadena.length;
+    }
+    catch (error) {
+        alert(error);
+        respuesta=-1;
+    }
+    return respuesta;
+};
+
+let caracteres = contarCaracteres(respuesta);
+
+if (caracteres>0) console.log (`La cadena ${respuesta} tiene una longitud de ${caracteres}`);
+ */
